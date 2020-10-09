3e328c528b85f4222c8ed1186439317e7e6d225d
Claire <clairebun_3@hotmail.com>
2020-10-09 13:29:37 -0400

    Update code - not optimized yet

--- Palindrome/src/com/palindrome/Main.java
+++ Palindrome/src/com/palindrome/Main.java
@@ -4,18 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
+    // Fonction qui cherche le PLP dans une chaine de char entree
+    static int trouverPLP(String chaineEntree) {
+        int something = 1;
+        return something;
+    }
+
     public static void main(String[] args) {
-        // source code: https://www.javatpoint.com/palindrome-program-in-java
-        String original, reverse = ""; // Objects of String class
+        String charEntree, palindrome = "";
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a string to check if it is a palindrome");
-        original = in.nextLine();
-        int length = original.length();
-        for ( int i = length - 1; i >= 0; i-- )
-            reverse = reverse + original.charAt(i);
-        if (original.equals(reverse))
-            System.out.println("Entered string is a palindrome.");
-        else
-            System.out.println("Entered string isn't a palindrome.");
+
+        // Demander a l'utilisateur d'entrer sa chaine de charactere
+        System.out.println("Enter a string to check if it is a palindrome: ");
+        charEntree = in.nextLine();
+        int palindromeLength = charEntree.length();
+        for ( int i = palindromeLength - 1; i >= 0; i--)
+            palindrome = palindrome + charEntree.charAt(i);
+        // Regarder la longeur de la chaine entree
+        if ( charEntree.equals(palindrome)) {
+            System.out.println("Longueur maximale de palindrome PLP = " + palindromeLength);
+//            System.out.print("Palindrome PLP trouvé = " + palindrome);
+            System.out.print("Palindrome PLP trouvé = " + trouverPLP(palindrome));
+        }
+        else {
+            System.out.println("La chaine " + charEntree + "ne correspond pas à un palindrome.");
+        }
     }
 }
+
+// Strings to tests
+// abcdbcmmcba
